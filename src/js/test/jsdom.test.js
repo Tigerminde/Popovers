8cc8fbfcc828ba0fe351fc22ac19594daba6e981
AppVeyor <ci@appveyor.com>
2020-04-20 13:37:50 +0300

    first commit

---
+++ src/js/test/jsdom.test.js
@@ -0,0 +1,23 @@
+import Popovers from '../../js/Popovers';
+
+test('Render self', () => {
+  const widget = new Popovers(document.body);
+  widget.bindToDOM();
+  const container = document.querySelector('#popup');
+
+  expect(container.innerHTML).toEqual(widget.htmlElement);
+});
+
+test('Show popup', () => {
+  const widget = new Popovers(document.body);
+  widget.bindToDOM();
+  const container = document.querySelector('#popup');
+
+  const docBody = document.body;
+  docBody.addEventListener('click', () => {
+    widget.positionPopup(docBody);
+  });
+  docBody.click();
+
+  expect(container.classList.contains('hidden')).toBeFalsy();
+});
---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
